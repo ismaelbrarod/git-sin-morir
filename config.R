891272210fb6d16a3559cab1c89b110c5dd5421b
ismael_bravo <ismael.bravo.rodriguez@gmail.com>
2026-08-17 10:45:22 -0400

    Cree el archivo config

---
+++ config.R
@@ -0,0 +1,25 @@
+# LIBRERÍAS ----
+
+## Instalar librerías ----
+
+install.packages("gitcreds")
+
+## Cargar librerías ----
+
+library(usethis)
+library(gitcreds)
+
+# CONFIGURACIÓN GIT ----
+
+## Archivo de configuración ----
+
+edit_git_config()
+
+# LLAVES GITHUB ----
+
+create_github_token()
+gitcreds_set()
+
+# REPOSITORIO LOCAL ----
+
+use_git()
